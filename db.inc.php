9dc3bc88a39c10f90f13fda74d572c973098a379
ASHISH PAL <143118293+ashishpal9@users.noreply.github.com>
2024-04-18 16:00:48 +0530

    Create db.inc.php

---
+++ db.inc.php
@@ -0,0 +1,6 @@
+ <?php
+ define('DB_SERVER', ' php-web-database.clcyei2mau6g.ap-south-1.rds.amazonaws.com');
+ define('DB_USERNAME', 'php_database_database');
+ define('DB_PASSWORD', '123456789');
+ define('DB_DATABASE', 'php_web_database');
+ ?>
